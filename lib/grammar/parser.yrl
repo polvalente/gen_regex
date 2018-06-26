545d5fb3b1fa6790be25e24885ae354a7a4530d8
Paulo Valente <polvalente@gmail.com>
2018-06-25 21:49:06 -0300

    feature: parser now supports more complex structures (removes need to hardcode 'wordlist' and 'listword') tests: update tests accordingly (and use keyword list notation)

--- lib/grammar/parser.yrl
+++ lib/grammar/parser.yrl
@@ -3,14 +3,13 @@ word
 list
 list_elems 
 elem 
+elems
 range
 set_elems
 set
 repzero
 reponce
-optelem
-wordlist
-listword.
+optelem.
 
 Terminals 
 '(' 
@@ -23,46 +22,42 @@ Terminals
 '+'
 '?'
 '.'
+''
 escape
 atom.
 
-Rootsymbol elem.
+Rootsymbol elems.
 
-elem -> word                     : {word, '$1'}.
-elem -> list                     : {list, '$1'}. 
-elem -> set                      : {set, '$1'}.
-elem -> repzero                  : {repzero, '$1'}.
-elem -> reponce                  : {reponce, '$1'}.
-elem -> optelem                  : {optelem, '$1'}.
-elem -> listword                 : {listword, '$1'}.
-elem -> wordlist                 : {wordlist, '$1'}.
-elem -> '.'                      : {wildcard, '.'}.
+elems -> elem                       : '$1'.
+elems -> elem elems                 : '$1' ++ '$2'.
 
-list_elems -> elem               : ['$1']. 
-list_elems -> elem '|' list_elems: ['$1' | '$3' ].
+elem -> word                        : [{word, '$1'}].
+elem -> list                        : [{list, '$1'}]. 
+elem -> set                         : [{set, '$1'}].
+elem -> repzero                     : ['$1'].
+elem -> reponce                     : ['$1'].
+elem -> optelem                     : ['$1'].
+elem -> '.'                         : [{wildcard, '.'}].
 
-set_elems -> elem                : ['$1']. 
-set_elems -> elem set_elems      : ['$1' | '$2' ].
+list -> '(' ')'                     : [].
+list -> '(' list_elems ')'          : '$2'.
+list_elems -> elem                  : '$1'.
+list_elems -> elem '|' list_elems   : '$1' ++ '$3'.
 
-list -> '(' ')'                  : [].
-list -> '(' list_elems ')'       : '$2'.
+set -> '[' ']'                      : [].
+set -> '[' elems ']'                : '$2'.
 
-set -> '[' ']'                   : [].
-set -> '[' set_elems ']'         : '$2'.
+word -> ''                          : [{atom, ''}].
+word -> atom                        : [{atom, extract_token('$1')}].
+word -> escape                      : [{escape, extract_token('$1')}].
+word -> range                       : ['$1'].
+word -> word word                   : '$1' ++ '$2'.
 
-word -> atom                     : {atom, extract_token('$1')}.
-word -> escape                   : {escape, extract_token('$1')}.
-word -> range                    : {range, '$1'}.
-word -> word word                : ['$1' | '$2'].
+range -> atom '-' atom              : {range, {extract_token('$1'), extract_token('$3')}}.
 
-range -> atom '-' atom           : {extract_token('$1'), extract_token('$3')}.
-
-repzero -> elem '*'              : '$1'.
-reponce -> elem '+'              : '$1'.
-optelem -> elem '?'              : '$1'.
-
-wordlist -> word list            : {'$1', '$2'}.
-listword -> list word            : {'$1', '$2'}.
+repzero -> elem '*'                 : {repzero, '$1'}.
+reponce -> elem '+'                 : {reponce, '$1'}.
+optelem -> elem '?'                 : {optelem, '$1'}.
 
 Erlang code.
 extract_token({_Token, _Line, Value}) -> Value.
--- test/parser_test.exs
+++ test/parser_test.exs
@@ -11,125 +11,134 @@ defmodule GenRegex.ParserTest do
 
   test "Should parse word (with atoms, range, escape)" do
     ast = parse(~r/aA0!@¨\w0-9/)
-    assert ast == {:word,
-      [
-        {:atom, 'a'},
-        {:atom, 'A'},
-        {:atom, '0'},
-        {:atom, '!'},
-        {:atom, '@'},
-        {:atom, [168]},
-        {:escape, '\\w'}|
-        {:range, {'0', '9'}}
+    assert ast == [
+      word: [
+        atom: 'a',
+        atom: 'A',
+        atom: '0',
+        atom: '!',
+        atom: '@',
+        atom: [168],
+        escape: '\\w',
+        range: {'0', '9'}
       ]
-    }
+    ]
   end
 
   test "Should parse list" do
     ast = parse(~r/(foo)/)
-    assert ast == {:list, [
-      {:word, [
-        {:atom, 'f'},
-        {:atom, 'o'}|
-        {:atom, 'o'}
-      ]}
-    ]}
+    assert ast == [
+      list: [
+        word: [
+          atom: 'f',
+          atom: 'o',
+          atom: 'o'
+        ]
+      ]
+    ]
   end
 
   test "Should parse set" do
     ast = parse(~r/[foo.]/)
-    assert ast == {:set, [
-      {:word, [
-        {:atom, 'f'},
-        {:atom, 'o'}|
-        {:atom, 'o'}
-      ]},
-      {:wildcard, :'.'}
-    ]}
+    assert ast == [
+      set: [
+        word: [
+          atom: 'f',
+          atom: 'o',
+          atom: 'o'
+        ],
+        wildcard: :.
+      ]
+    ]
   end
 
   test "Should parse *" do
     ast = parse(~r/a*/)
-    assert ast == {:repzero,
-      {:word,
-        {:atom, 'a'}
-      }
-    }
+    assert ast == [
+      repzero: [
+        word: [
+          atom: 'a'
+        ]
+      ]
+    ]
   end
 
   test "Should parse +" do
     ast = parse(~r/a+/)
-    assert ast == {:reponce,
-      {:word,
-        {:atom, 'a'}
-      }
-    }
+    assert ast == [
+      reponce: [
+        word: [
+          atom: 'a'
+        ]
+      ]
+    ]
   end
 
   test "Should parse ?" do
     ast = parse(~r/a?/)
-    assert ast == {:optelem,
-      {:word,
-        {:atom, 'a'}
-      }
-    }
+    assert ast == [
+      optelem: [
+        word: [
+          atom: 'a'
+        ]
+      ]
+    ]
   end
 
   test "Should parse wildcard" do
     ast = parse(~r/./)
-    assert ast == {:wildcard, :.}
+    assert ast == [wildcard: :.]
   end
 
-  test "Should parse listword" do
+  test "Should parse list+word" do
     ast = parse(~r/(first|last)_name/)
-    assert ast == {:listword,{
-      [
-        {:word, [
-          {:atom, 'f'},
-          {:atom, 'i'},
-          {:atom, 'r'},
-          {:atom, 's'}|
-          {:atom, 't'}
-        ]},
-        {:word, [
-          {:atom, 'l'},
-          {:atom, 'a'},
-          {:atom, 's'}|
-          {:atom, 't'}
-        ]}
+    assert ast == [
+      list: [
+        word: [
+          atom: 'f',
+          atom: 'i',
+          atom: 'r',
+          atom: 's',
+          atom: 't'
+        ],
+        word: [
+          atom: 'l',
+          atom: 'a',
+          atom: 's',
+          atom: 't'
+        ]
       ],
-      [
-        {:atom, '_'},
-        {:atom, 'n'},
-        {:atom, 'a'},
-        {:atom, 'm'}|
-        {:atom, 'e'}
+      word: [
+        atom: '_',
+        atom: 'n',
+        atom: 'a',
+        atom: 'm',
+        atom: 'e'
       ]
-    }}
+    ]
   end
 
-  test "Should parse wordlist" do
+  test "Should parse word+list" do
     ast = parse(~r/foo_(bar|baz)/)
-    assert ast == {:wordlist,{
-      [
-        {:atom, 'f'},
-        {:atom, 'o'},
-        {:atom, 'o'}|
-        {:atom, '_'}
+    assert ast == [
+      word: [
+        atom: 'f',
+        atom: 'o',
+        atom: 'o',
+        atom: '_'
       ],
-      [
-        {:word, [
-          {:atom, 'b'},
-          {:atom, 'a'}|
-          {:atom, 'r'}
-        ]},
-        {:word, [
-          {:atom, 'b'},
-          {:atom, 'a'}|
-          {:atom, 'z'}
-        ]}
-      ],
-
-    }}
+      list: [
+        word: [
+          atom: 'b',
+          atom: 'a',
+          atom: 'r'
+        ],
+        word: [
+          atom: 'b',
+          atom: 'a',
+          atom: 'z'
+        ]
+      ]
+    ]
   end
 end
